40b3bce47dc6a19b2760bcda04eeb4baafad4cf3
tech2188 <tsheehan.datapro@gmail.com>
2023-06-24 13:54:55 -0400

    adding python work

--- notes.R
+++ notes.R
@@ -1,7 +1,12 @@
 distill::create_blog(dir = "TimSheehanDataPro", title = "Tim Sheehan Data Professional")
 library(distill)
 
-distill::create_post("My Doggies")
+distill::create_post("Playing With Python")
 
 
 rmarkdown::render_site()
+
+install.packages("Anaconda")
+install.packages("reticulate")
+
+Sys.which("python")
